95c3d6d2e159519b3826302740c00897ba50ab44
Sanju Mondal <sanjumondal130105@gmail.com>
2024-06-19 12:10:26 +0530

    step feature, bento grid, customer stories pricing section, faq and footer added

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const handleMenu = () => {
   navDialog.classList.toggle("hidden");
 };
 
+
+//icon left right transition part
 const initialTranslateLTR = -48 * 4;
 const initialTranslateRTL = 36 * 4;
 
@@ -32,10 +34,24 @@ const setupIntersectionObserver = (element, isLTR, speed) => {
   }
 
 }
-
 const line1 = document.getElementById("line1");
 const line2 = document.getElementById("line2");
 const line3 = document.getElementById("line3");
+const line4 = document.getElementById("line4");
 setupIntersectionObserver(line1, true, 0.15);
 setupIntersectionObserver(line2, false, 0.15);
 setupIntersectionObserver(line3, true, 0.15);
+setupIntersectionObserver(line4, true, 0.8);
+
+//FAQ Part
+const dtElements = document.querySelectorAll("dt");
+dtElements.forEach(element => {
+  element.addEventListener("click", () => {
+    const ddId = element.getAttribute("aria-controls");
+    const ddElement = document.getElementById(ddId);
+    const ddArrowIcon = element.querySelectorAll("i")[0];
+
+    ddElement.classList.toggle("hidden");
+    ddArrowIcon.classList.toggle("-rotate-180")
+  })
+})
